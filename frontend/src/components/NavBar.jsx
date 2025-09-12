25fdd5ba6967df75cd948b50d54896852c8f53bd
Roshan Patel <roshanpatel12309@gmail.com>
2025-09-13 01:15:08 +0530

    Added all item of navBar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -40,22 +40,22 @@ const NavBar = () => {
   }
 
   return (
-    <div className='w-full flex justify-between items-center p-4 sm:p-4 sm:px-20 absolute top-0'>
+    <div className='bg-slate-900 w-full flex justify-between sm:py-3 sm:px-5 items-center absolute top-0'>
 
        <div className='flex items-center'>
         {/* <img src={assets.logo} alt="" className='w-25 sm:w-20' /><span className=' text-gray-800 font-semibold text-3xl sm:text-4xl pt-3'>Student_Registration</span> */}
     
-       <p className=' text-gray-800 font-semibold text-3xl sm:text-2xl pt-3'>Student_Reg.</p>
+       <p className='text-white font-semibold text-3xl sm:text-2xl mx-5'>P.K Arts College</p>
         </div>
 
         { userData ? 
-        <div className='w-11 h-11 flex justify-center items-center rounded-full bg-black text-white relative group'>
+        <div className='w-11 h-11 mx-5 flex justify-center items-center rounded-full bg-white text-slate-900 relative group'>
          {userData.name[0].toUpperCase()}
          <div className='absolute hidden group-hover:block top-0 right-0 z-19 text-black rounded pt-10'>
-            <ul className='list-none m-1 py-5 px-3 bg-gray-100 text-sm'>
-              {!userData.isAccountVerified && 
+            <ul className='list-none m-1 py-2 px-2 bg-gray-100 text-sm'>
+              {/* {!userData.isAccountVerified && 
               <li onClick={sendVerificationOtp} className='py-1 px-2 hover:bg-gray-200 cursor-pointer font-semibold'>Verify email</li> 
-              }
+              } */}
               <li onClick={logout} className='py-1 px-2 hover:bg-gray-200 cursor-pointer pr-10 font-semibold'>Logout</li>
             </ul>
           </div>
