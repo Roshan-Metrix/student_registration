f886ce5cd9e5c02679dc2eb8b4abaf6dadd71822
Roshan Patel <roshanpatel12309@gmail.com>
2025-09-13 16:01:52 +0530

    Update NavBar bg

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -62,7 +62,7 @@ const NavBar = () => {
         </div> : 
         <button 
         onClick={() => navigate('/login')}
-        className='flex items-center gap-2 border border-gray-500 rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100 transition-all cursor-pointer'>Login <img src={assets.arrow_icon} alt='' /></button>
+        className='flex items-center gap-2 border border-gray-500 rounded-full px-6 py-2 text-slate-900 bg-white hover:bg-gray-100 transition-all cursor-pointer'>Login <img src={assets.arrow_icon} alt='' /></button>
         }
 
 
