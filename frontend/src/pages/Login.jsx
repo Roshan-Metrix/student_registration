79171d7b567bc48ecda52a02a1fb85d35db0a81a
Roshan Patel <roshanpatel12309@gmail.com>
2025-09-13 16:53:52 +0530

    Updated login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,131 +1,178 @@
-import { useContext, useState } from 'react'
-import { assets } from '../assets/assets'
-import { useNavigate } from 'react-router-dom'
-import { AppContent } from '../context/AppContext';
-import axios from 'axios'
-import { toast } from 'react-toastify'
+import { useContext, useState } from "react";
+import { assets } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
+import { AppContent } from "../context/AppContext";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const Login = () => {
- 
- const navigate = useNavigate();
+  const navigate = useNavigate();
 
- const { backendUrl, setIsLoggedin, getUserData } = useContext(AppContent);
+  const { backendUrl, setIsLoggedin, getUserData } = useContext(AppContent);
 
+  const [state, setState] = useState("Sign Up");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
- const [state, setState] = useState('Sign Up')
- const [name, setName] = useState('')
- const [email, setEmail] = useState('')
- const [password, setPassword] = useState('')
+  const onSubmitHandler = async (e) => {
+    try {
+      e.preventDefault();
 
- const onSubmitHandler = async (e) => {
-  try{
-    e.preventDefault();
+      axios.defaults.withCredentials = true;
 
-    axios.defaults.withCredentials = true;
+      if (state === "Sign Up") {
+        const { data } = await axios.post(backendUrl + "/api/auth/register", {
+          name,
+          email,
+          password,
+        });
 
-    if(state === 'Sign Up'){
-      const {data} = await axios.post(backendUrl + '/api/auth/register',{name,email,password})
+        if (data.success) {
+          setIsLoggedin(true);
+        } else {
+          toast.error(data.message);
+        }
+      } else {
+        const { data } = await axios.post(backendUrl + "/api/auth/login", {
+          email,
+          password,
+        });
 
-      if(data.success){
-        setIsLoggedin(true)
-      }else{
-        toast.error(data.message)
+        if (data.success) {
+          setIsLoggedin(true);
+          getUserData();
+          navigate("/");
+        } else {
+          toast.error(data.message);
+        }
       }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
 
-    }else{
-         const {data} = await axios.post(backendUrl + '/api/auth/login',{email,password})
+  const sendVerificationOtp = async () => {
+    try {
+      axios.defaults.withCredentials = true;
+      const { data } = await axios.post(
+        backendUrl + "/api/auth/send-verify-otp"
+      );
 
-      if(data.success){
-        setIsLoggedin(true)
-        getUserData()
-        navigate('/')
-      }else{
-        toast.error(data.message)
+      if (data.success) {
+        navigate("/email-verify");
+        toast.success(data.message);
+      } else {
+        toast.error(data.message);
       }
+    } catch (error) {
+      toast.error(error.message);
     }
-  }catch(error){
-    toast.error(error.message)
-  }
- }
-
-const sendVerificationOtp = async () => {
-    try{
-       axios.defaults.withCredentials = true;
-       const { data } = await axios.post(backendUrl + '/api/auth/send-verify-otp')
-
-       if(data.success){
-         navigate('/email-verify')
-         toast.success(data.message)
-       }else{
-        toast.error(data.message)
-       }
-
-    } catch(error){
-      toast.error(error.message)
-    }
-  }
+  };
 
   return (
-    <div className='flex items-center justify-center min-h-screen bg-gray-100 px-6 sm:px-0 '>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100 px-6 sm:px-0 ">
+      <div className="bg-slate-900 w-full flex justify-between sm:py-3 sm:px-5 items-center absolute top-0">
+        <div onClick={() => navigate("/")} className="flex items-center">
+          {/* <img src={assets.logo} alt="" className='w-25 sm:w-20' /> */}
 
-      <div onClick={() => navigate('/')} className='cursor-pointer flex absolute top-3 sm:left-5 left-3'>
-        {/* <img  src={assets.logo} alt='' className='w-22 sm:w-25 ' /> */}
-        <p className=' text-slate-90 font-semibold text-3xl sm:text-2xl pt-3'>P.K Arts College</p>
+          <p className="text-white font-semibold text-3xl sm:text-2xl mx-5">
+            P.K Arts College
+          </p>
+        </div>
       </div>
-       
-      <div className='bg-slate-900 p-10 rounded-lg shadow-lg w-full sm:w-96 text-indigo-300 text-sm'>
-        <h2 className='text-3xl font-semibold text-white text-center mb-3'>{state === 'Sign Up' ? 'Create account' : 'Login'}</h2>
-        <p className='text-sm mb-6 text-center'>{state === 'Sign Up' ? 'Create your account' : 'Login to your account!'}</p>
+
+      <div className="bg-slate-900 p-10 rounded-lg shadow-lg w-full sm:w-96 text-indigo-300 text-sm">
+        <h2 className="text-3xl font-semibold text-white text-center mb-3">
+          {state === "Sign Up" ? "Create account" : "Login"}
+        </h2>
+        <p className="text-sm mb-6 text-center">
+          {state === "Sign Up"
+            ? "Create your account"
+            : "Login to your account!"}
+        </p>
         <form
           onSubmit={async (e) => {
             await onSubmitHandler(e);
-            if (state === 'Sign Up') {
+            if (state === "Sign Up") {
               await sendVerificationOtp();
             }
           }}
         >
-          {state === 'Sign Up' && (
-            <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
+          {state === "Sign Up" && (
+            <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
               <img src={assets.person_icon} alt="" />
-              <input className='bg-transparent outline-none placeholder-gray-300' 
-                onChange={(e) => setName(e.target.value)} value={name} 
-                type="text" 
-                placeholder='Full Name' required />
+              <input
+                className="bg-transparent outline-none placeholder-gray-300"
+                onChange={(e) => setName(e.target.value)}
+                value={name}
+                type="text"
+                placeholder="Full Name"
+                required
+              />
             </div>
           )}
-         
-          <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
+
+          <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
             <img src={assets.mail_icon} alt="" />
-            <input className='bg-transparent outline-none placeholder-gray-300' type="email" 
+            <input
+              className="bg-transparent outline-none placeholder-gray-300"
+              type="email"
               onChange={(e) => setEmail(e.target.value)}
-              value={email} 
-              placeholder='Email id' required />
+              value={email}
+              placeholder="Email id"
+              required
+            />
           </div>
 
-          <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
+          <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
             <img src={assets.lock_icon} alt="" />
-            <input className='bg-transparent outline-none placeholder-gray-300' type="password" 
-              onChange={(e) => setPassword(e.target.value)} 
-              value={password} placeholder='Password' required />
+            <input
+              className="bg-transparent outline-none placeholder-gray-300"
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              placeholder="Password"
+              required
+            />
           </div>
 
-          <p onClick={() => navigate('/reset-password')} className='mb-4 text-indigo-500 cursor-pointer'>Forgot Password ?</p>
+          <p
+            onClick={() => navigate("/reset-password")}
+            className="mb-4 text-indigo-500 cursor-pointer"
+          >
+            Forgot Password ?
+          </p>
 
-          <button className='w-full py-2.5 rounded-full bg-gradient-to-r from-indigo-500 to-indigo-900 text-white font-medium cursor-pointer'>{state}</button>
+          <button className="w-full py-2.5 rounded-full bg-gradient-to-r from-indigo-500 to-indigo-900 text-white font-medium cursor-pointer">
+            {state}
+          </button>
         </form>
 
-        {state === 'Sign Up' ? (
-          <p className='text-gray-400 text-center text-sm mt-4'>Already have a account?{'  '}
-            <span onClick={() => setState('Login')} className='text-blue-400 cursor-pointer underline'>Login here</span>
+        {state === "Sign Up" ? (
+          <p className="text-gray-400 text-center text-sm mt-4">
+            Already have a account?{"  "}
+            <span
+              onClick={() => setState("Login")}
+              className="text-blue-400 cursor-pointer underline"
+            >
+              Login here
+            </span>
           </p>
         ) : (
-          <p className='text-gray-400 text-center text-sm mt-4'>Don't have an account?{'  '}
-            <span onClick={() => setState('Sign Up')} className='text-blue-400 cursor-pointer underline'>Sign Up</span>
+          <p className="text-gray-400 text-center text-sm mt-4">
+            Don't have an account?{"  "}
+            <span
+              onClick={() => setState("Sign Up")}
+              className="text-blue-400 cursor-pointer underline"
+            >
+              Sign Up
+            </span>
           </p>
         )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
