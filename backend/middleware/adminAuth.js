ebc4a6f4986d66ed3a5b309baa202acaed54e6f4
Roshan Patel <roshanpatel12309@gmail.com>
2025-09-16 08:01:39 +0530

    fixes issues

--- backend/middleware/adminAuth.js
+++ backend/middleware/adminAuth.js
@@ -1,30 +1,30 @@
-
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 const adminAuth = async (req, res, next) => {
-   const { token } = req.cookies;
+  const { token } = req.cookies;
 
-   if(!token){
-    return res.json({success:false,message:'Not Authorized. Login Again'})
-   }
+  if (!token) {
+    return res.json({ success: false, message: "Not Authorized. Login Again" });
+  }
 
-try {
+  try {
     // decode & store token
     const decodeToken = jwt.verify(token, process.env.JWT_SECRET);
 
-    if (decodeToken && decodeToken.role === 'admin') {
-        req.role = 'admin';
+    if (decodeToken && decodeToken.role === "admin") {
+      req.role = "admin";
     } else {
-        return res.json({ success: false, message: "Not Authorized. Admin access required." });
+      return res.json({
+        success: false,
+        message: "Not Authorized. Admin access required.",
+      });
     }
 
     next();
-
-} catch (error) {
-    console.log("Error in adminAuth middleware",error);
+  } catch (error) {
+    console.log("Error in adminAuth middleware", error);
     res.json({ success: false, message: error.message });
-}
-   
-}
+  }
+};
 
 export default adminAuth;
