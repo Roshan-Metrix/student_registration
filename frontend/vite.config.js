e61b26abbd8482c495ebb3d84dffccdddd0a138a
Roshan Patel <roshanpatel12309@gmail.com>
2025-10-06 00:08:04 +0530

    Redirect issue fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,15 +6,8 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  build: {
-    outDir: 'dist', 
+   build: {
+    outDir: 'dist',
   },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  server: {
-    historyApiFallback: true,
-  },
+  publicDir: 'public'
 })
