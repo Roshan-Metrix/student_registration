d23a40c38963504cca5eb168e3c42429bc5bc98a
Roshan Patel <roshanpatel12309@gmail.com>
2025-10-05 15:11:05 +0530

    Added new controller to get more students details

--- backend/controllers/rolesController.js
+++ backend/controllers/rolesController.js
@@ -32,67 +32,9 @@ export const studentDetail = async (req, res) => {
 
     const photo = req.file ? req.file.filename : null;
 
-    // if (!req.file || !["image/jpeg", "image/png"].includes(req.file.mimetype)) {
-    //   return res
-    //     .status(400)
-    //     .json({ success: false, message: "Invalid image format" });
-    // }
-
-    //if hostler is false then they should write detail so the hostler is boolean and hostlerdetail is text
-
-    //  const userId = req.userId;
-
-    //    if (!name || !dob || !fatherName || !fatherOccupation || !motherName || !motherOccupation || !mediumOfInstruction || !marksScored || !percentage ||!schoolNamePlace || !religion || !nationality || !category || !dateOfAdmission ||!dateOfLeaving || !contactNo || !email || !aadhaar || !address || !gender || !course || !year || !photo || !bloodGroup || !scholarshipDetails ){
-    //  return res.json({ success: false, message: "All fields are required" });
-    //      }
-
     const db = (await createDB.getConnection)
       ? await createDB.getConnection()
       : await createDB();
-
-    // This is to check and give only student to add access
-    // const [userRows] = await db.execute('SELECT * FROM users WHERE id = ?', [userId]);
-    // if (userRows.length === 0) {
-    //     return res.json({ success: false, message: "User not found" });
-    // }
-    // const user = userRows[0];
-
-    // if(user.role !== 'student'){
-    //     return res.json({ success: false, message: "Access denied. Students only." });
-    // }
-
-    const query = `CREATE TABLE IF NOT EXISTS studentdata (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            student_uid VARCHAR(10),
-            name VARCHAR(100),
-            dob DATE,
-            fatherName VARCHAR(100),
-            fatherOccupation VARCHAR(100),
-            motherName VARCHAR(100),
-            motherOccupation VARCHAR(100),
-            mediumOfInstruction VARCHAR(50),
-            marksScored INT,
-            percentage INT,
-            schoolNamePlace VARCHAR(100),
-            religion VARCHAR(50),
-            nationality VARCHAR(50),
-            category VARCHAR(50),
-            dateOfAdmission DATE,
-            dateOfLeaving DATE,
-            contactNo VARCHAR(15),
-            email VARCHAR(100),
-            aadhaar VARCHAR(50),
-            address TEXT,
-            gender VARCHAR(10),
-            course VARCHAR(100),
-            year INT,
-            photo VARCHAR(255),
-            bloodGroup VARCHAR(10),
-            scholarshipDetails VARCHAR(100),
-            createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-        )`;
-
-    await db.execute(query);
 
     // Insert student data
     const insertQuery =
@@ -150,10 +92,108 @@ export const studentDetail = async (req, res) => {
 
     await generateStudentId(studentId[0].id);
 
-    res.json({ success: true, message: "Student data inserted successfully." });
+    const [student_uid] = await db.execute(
+      "SELECT student_uid FROM studentdata WHERE email = ?",
+      [email ?? null]
+    );
+
+    res.json({ success: true, message: "Student data inserted successfully.", student_uid: student_uid[0].student_uid });
   } catch (error) {
     console.log("Error in studentDetail controller", error);
     res.json({ success: false, message: error.message });
+  }
+};
+
+export const storeExtraStudentData = async (req, res) => {
+  try {
+    const { student_uid } = req.params;
+    const data = req.body;
+    const db = (await createDB.getConnection)
+      ? await createDB.getConnection()
+      : await createDB();
+
+    // Check if student exists
+    const [existingFees] = await db.execute(
+      "SELECT * FROM student_fees WHERE student_uid = ?",
+      [student_uid]
+    );
+
+    if(!existingFees) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Student not found." });
+    }
+
+      // INSERT new record
+    await db.execute(
+        "INSERT INTO student_fees (student_uid, feesYear1, feesYear2, feesYear3, feesYear4) VALUES (?, ?, ?, ?, ?)",
+        [
+          student_uid,
+          data.feesYear1 ?? null,
+          data.feesYear2 ?? null,
+          data.feesYear3 ?? null,
+          data.feesYear4 ?? null,
+        ]
+      );
+
+        await db.execute(
+        "INSERT INTO student_attendance (student_uid, attendanceSem1, attendanceSem2, attendanceSem3, attendanceSem4, attendanceSem5, attendanceSem6, attendanceSem7, attendanceSem8) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+        [
+          student_uid,
+          data.attendanceSem1 ?? null,
+          data.attendanceSem2 ?? null,
+          data.attendanceSem3 ?? null,
+          data.attendanceSem4 ?? null,
+          data.attendanceSem5 ?? null,
+          data.attendanceSem6 ?? null,
+          data.attendanceSem7 ?? null,
+          data.attendanceSem8 ?? null,
+        ]
+      );
+
+      await db.execute(
+        "INSERT INTO student_semesters (student_uid, examfeesSem1, examfeesSem2, examfeesSem3, examfeesSem4, examfeesSem5, examfeesSem6, examfeesSem7, examfeesSem8, gpaSem1, gpaSem2, gpaSem3, gpaSem4, gpaSem5, gpaSem6, gpaSem7, gpaSem8, cgpaSem1, cgpaSem2, cgpaSem3, cgpaSem4, cgpaSem5, cgpaSem6, cgpaSem7, cgpaSem8, marksheetSem1, marksheetSem2, marksheetSem3, marksheetSem4, marksheetSem5, marksheetSem6, marksheetSem7, marksheetSem8) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+        [
+          student_uid,
+          data.examfeesSem1 ?? null,
+          data.examfeesSem2 ?? null,
+          data.examfeesSem3 ?? null,
+          data.examfeesSem4 ?? null,
+          data.examfeesSem5 ?? null,
+          data.examfeesSem6 ?? null,
+          data.examfeesSem7 ?? null,
+          data.examfeesSem8 ?? null,
+          data.gpaSem1 ?? null,
+          data.gpaSem2 ?? null,
+          data.gpaSem3 ?? null,
+          data.gpaSem4 ?? null,
+          data.gpaSem5 ?? null,
+          data.gpaSem6 ?? null,
+          data.gpaSem7 ?? null,
+          data.gpaSem8 ?? null,
+          data.cgpaSem1 ?? null,
+          data.cgpaSem2 ?? null,
+          data.cgpaSem3 ?? null,
+          data.cgpaSem4 ?? null,
+          data.cgpaSem5 ?? null,
+          data.cgpaSem6 ?? null,
+          data.cgpaSem7 ?? null,
+          data.cgpaSem8 ?? null,
+          data.marksheetSem1 ?? null,
+          data.marksheetSem2 ?? null,
+          data.marksheetSem3 ?? null,
+          data.marksheetSem4 ?? null,
+          data.marksheetSem5 ?? null,
+          data.marksheetSem6 ?? null,
+          data.marksheetSem7 ?? null,
+          data.marksheetSem8 ?? null,
+        ]
+      );
+ 
+    res.json({ success: true, message: "Student data saved successfully." });
+  } catch (error) {
+    console.error("Error in storeExtraStudentData:", error);
+    res.status(500).json({ success: false, message: error.message });
   }
 };
 
@@ -198,6 +238,7 @@ export const viewStudentData = async (req, res) => {
       ? await createDB.getConnection()
       : await createDB();
 
+      // Check if student exists
     const [studentRows] = await db.execute(
       "SELECT * FROM studentdata WHERE student_uid = ?",
       [student_uid]
@@ -209,8 +250,21 @@ export const viewStudentData = async (req, res) => {
         .json({ success: false, message: "Student not found" });
     }
 
+    const [student_fees] = await db.execute(
+      "SELECT * FROM student_fees WHERE student_uid = ?",
+      [student_uid]
+    );
+    const [student_attendance] = await db.execute(
+      "SELECT * FROM student_attendance WHERE student_uid = ?",
+      [student_uid]
+    );
+    const [student_semesters] = await db.execute(
+      "SELECT * FROM student_semesters WHERE student_uid = ?",
+      [student_uid]
+    );
+
     const student = studentRows[0];
-    return res.status(200).json({ success: true, student });
+    return res.status(200).json({ success: true, student, student_fees, student_attendance, student_semesters });
   } catch (error) {
     console.error("Error in viewStudentData controller:", error);
     return res
@@ -246,12 +300,11 @@ export const updateStudentDetail = async (req, res) => {
       gender,
       course,
       year,
-      photo,
       bloodGroup,
-      scholarshipDetails,
-      hosteller,
-      hostellerDetail,
+      scholarshipDetails
     } = req.body;
+    
+    const photo = req.file ? req.file.filename : null;
 
     const db = (await createDB.getConnection)
       ? await createDB.getConnection()
@@ -270,36 +323,34 @@ export const updateStudentDetail = async (req, res) => {
 
     // Update student data
     const updateQuery = `
-            UPDATE studentdata SET
-            name = ?,
-            dob = ?,
-            fatherName = ?,
-            fatherOccupation = ?,
-            motherName = ?,
-            motherOccupation = ?,
-            mediumOfInstruction = ?,
-            marksScored = ?,
-            percentage = ?,
-            schoolNamePlace = ?,
-            religion = ?,
-            nationality = ?,
-            category = ?,
-            dateOfAdmission = ?,
-            dateOfLeaving = ?,
-            contactNo = ?,
-            email = ?,
-            aadhaar = ?,
-            address = ?,
-            gender = ?,
-            course = ?,
-            year = ?,
-            photo = ?,
-            bloodGroup = ?,
-            scholarshipDetails = ?,
-            hosteller = ?,
-            hostellerDetail = ?
-            WHERE student_uid = ?
-        `;
+    UPDATE studentdata SET
+    name = ?,
+    dob = ?,
+    fatherName = ?,
+    fatherOccupation = ?,
+    motherName = ?,
+    motherOccupation = ?,
+    mediumOfInstruction = ?,
+    marksScored = ?,
+    percentage = ?,
+    schoolNamePlace = ?,
+    religion = ?,
+    nationality = ?,
+    category = ?,
+    dateOfAdmission = ?,
+    dateOfLeaving = ?,
+    contactNo = ?,
+    email = ?,
+    aadhaar = ?,
+    address = ?,
+    gender = ?,
+    course = ?,
+    year = ?,
+    photo = ?,
+    bloodGroup = ?,
+    scholarshipDetails = ?
+    WHERE student_uid = ?
+`;
 
     const studentParams = [
       name ?? null,
@@ -327,9 +378,7 @@ export const updateStudentDetail = async (req, res) => {
       photo ?? null,
       bloodGroup ?? null,
       scholarshipDetails ?? null,
-      hosteller ?? null,
-      hostellerDetail ?? null,
-      student_uid,
+      student_uid
     ];
 
     await db.execute(updateQuery, studentParams);
@@ -338,6 +387,101 @@ export const updateStudentDetail = async (req, res) => {
   } catch (error) {
     console.log("Error in studentAccess controller", error);
     res.json({ success: false, message: error.message });
+  }
+};
+
+export const updateExtraStudentData = async (req, res) => {
+  try {
+    const { student_uid } = req.params;
+    const data = req.body;
+    const db = (await createDB.getConnection)
+      ? await createDB.getConnection()
+      : await createDB();
+
+    // Check if student exists
+    const [existingFees] = await db.execute(
+      "SELECT * FROM student_fees WHERE student_uid = ?",
+      [student_uid]
+    );
+
+    if(!existingFees) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Student not found." });
+    }
+
+      // UPDATE existing data
+    await db.execute(
+        `UPDATE student_fees SET feesYear1=?, feesYear2=?, feesYear3=?, feesYear4=? WHERE student_uid=?`,
+        [
+          data.feesYear1 ?? null,
+          data.feesYear2 ?? null,
+          data.feesYear3 ?? null,
+          data.feesYear4 ?? null,
+          student_uid,
+        ]
+      );
+
+    await db.execute(
+        `UPDATE student_attendance SET attendanceSem1=?, attendanceSem2=?, attendanceSem3=?, attendanceSem4=?, attendanceSem5=?, attendanceSem6=?, attendanceSem7=?, attendanceSem8=? WHERE student_uid=?`,
+        [
+          data.attendanceSem1 ?? null,
+          data.attendanceSem2 ?? null,
+          data.attendanceSem3 ?? null,
+          data.attendanceSem4 ?? null,
+          data.attendanceSem5 ?? null,
+          data.attendanceSem6 ?? null,
+          data.attendanceSem7 ?? null,
+          data.attendanceSem8 ?? null,
+          student_uid,
+        ]
+      );
+
+     await db.execute(
+        `UPDATE student_semesters SET examfeesSem1=?, examfeesSem2=?, examfeesSem3=?, examfeesSem4=?, examfeesSem5=?, examfeesSem6=?, examfeesSem7=?, examfeesSem8=?, 
+        gpaSem1=?, gpaSem2=?, gpaSem3=?, gpaSem4=?, gpaSem5=?, gpaSem6=?, gpaSem7=?, gpaSem8=?, 
+        cgpaSem1=?, cgpaSem2=?, cgpaSem3=?, cgpaSem4=?, cgpaSem5=?, cgpaSem6=?, cgpaSem7=?, cgpaSem8=? ,marksheetSem1=?, marksheetSem2=?,marksheetSem3=?, marksheetSem4=?,marksheetSem5=?, marksheetSem6=?,marksheetSem7=?, marksheetSem8=?  WHERE student_uid=?`,
+        [
+          data.examfeesSem1 ?? null,
+          data.examfeesSem2 ?? null,
+          data.examfeesSem3 ?? null,
+          data.examfeesSem4 ?? null,
+          data.examfeesSem5 ?? null,
+          data.examfeesSem6 ?? null,
+          data.examfeesSem7 ?? null,
+          data.examfeesSem8 ?? null,
+          data.gpaSem1 ?? null,
+          data.gpaSem2 ?? null,
+          data.gpaSem3 ?? null,
+          data.gpaSem4 ?? null,
+          data.gpaSem5 ?? null,
+          data.gpaSem6 ?? null,
+          data.gpaSem7 ?? null,
+          data.gpaSem8 ?? null,
+          data.cgpaSem1 ?? null,
+          data.cgpaSem2 ?? null,
+          data.cgpaSem3 ?? null,
+          data.cgpaSem4 ?? null,
+          data.cgpaSem5 ?? null,
+          data.cgpaSem6 ?? null,
+          data.cgpaSem7 ?? null,
+          data.cgpaSem8 ?? null,
+          data.marksheetSem1 ?? null,
+          data.marksheetSem2 ?? null,
+          data.marksheetSem3 ?? null,
+          data.marksheetSem4 ?? null,
+          data.marksheetSem5 ?? null,
+          data.marksheetSem6 ?? null,
+          data.marksheetSem7 ?? null,
+          data.marksheetSem8 ?? null,
+          student_uid,
+        ]
+      );
+
+    res.json({ success: true, message: "Student fee's,attendance and semester details updated successfully." });
+  } catch (error) {
+    console.error("Error in updateExtraStudentData:", error);
+    res.status(500).json({ success: false, message: error.message });
   }
 };
 
