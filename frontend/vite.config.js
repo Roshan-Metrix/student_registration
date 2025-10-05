1d853a8be19fefc97c9d04684f8a58180dc36421
Roshan Patel <roshanpatel12309@gmail.com>
2025-10-06 00:20:40 +0530

    Redirect issue fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-   build: {
-    outDir: 'dist',
-  },
-  publicDir: 'public'
+  publicDir: 'public', 
 })
