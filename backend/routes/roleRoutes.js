1c16ef1a853a1d8739283c9232e11b114f2121f1
Roshan Patel <roshanpatel12309@gmail.com>
2025-10-12 20:51:53 +0530

    Modified multer image saving

--- backend/routes/roleRoutes.js
+++ backend/routes/roleRoutes.js
@@ -1,33 +1,34 @@
 import express from 'express';
 import multer from 'multer';
-import userAuth from '../middleware/userAuth.js'
-import { addCoursesAndYears, deleteStudent, getCoursesAndYears, getLoggedInUserData, storeExtraStudentData, studentDetail, updateExtraStudentData, updateStudentDetail, viewAllStudentsData, viewStudentData } from '../controllers/rolesController.js';
+import userAuth from '../middleware/userAuth.js';
 import adminAuth from '../middleware/adminAuth.js';
+import {
+  addCoursesAndYears,
+  deleteStudent,
+  getCoursesAndYears,
+  getLoggedInUserData,
+  storeExtraStudentData,
+  studentDetail,
+  updateExtraStudentData,
+  updateStudentDetail,
+  viewAllStudentsData,
+  viewStudentData,
+} from '../controllers/rolesController.js';
 
-// multer setup for photo
-const storage = multer.diskStorage({
-    destination: function (req,file,cb){
-        cb(null, "uploads/");
-    },
-    filename: function (req,file,cb){
-        const uniqueName = Date.now() + "-" + file.originalname.toLowerCase().replace(/\s+/g, "_").replace(/[^a-z0-9._-]/g, "");
-
-        cb(null,uniqueName);
-    }
-}); // store image in memory as buffer
-const upload = multer({ storage });
+// multer setup for storing image in memory (as buffer)
+const upload = multer({ storage: multer.memoryStorage() });
 
 const roleRouter = express.Router();
 
-roleRouter.post('/students',userAuth,upload.single('photo'),studentDetail);
-roleRouter.post('/students/moreData/:student_uid',userAuth,storeExtraStudentData);
-roleRouter.get('/getStudentsData',userAuth,viewAllStudentsData);
+roleRouter.post('/students', userAuth, upload.single('photo'), studentDetail);
+roleRouter.post('/students/moreData/:student_uid', userAuth, storeExtraStudentData);
+roleRouter.get('/getStudentsData', userAuth, viewAllStudentsData);
 roleRouter.get('/viewStudentData/:student_uid', userAuth, viewStudentData);
-roleRouter.put('/updateStudentData/:student_uid', userAuth,upload.single('photo'), updateStudentDetail);
+roleRouter.put('/updateStudentData/:student_uid', userAuth, upload.single('photo'), updateStudentDetail);
 roleRouter.put('/updateExtraStudentData/:student_uid', userAuth, updateExtraStudentData);
 roleRouter.delete('/deleteStudent/:student_uid', userAuth, deleteStudent);
-roleRouter.get('/user/data', userAuth,getLoggedInUserData);
-roleRouter.get('/get-courses-years', userAuth,getCoursesAndYears);
+roleRouter.get('/user/data', userAuth, getLoggedInUserData);
+roleRouter.get('/get-courses-years', userAuth, getCoursesAndYears);
 roleRouter.post('/admin/add-courses-and-years', userAuth, adminAuth, addCoursesAndYears);
 
 export default roleRouter;
