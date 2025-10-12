24bc75ed5da1e6f627d71facd0ebe78517374783
Roshan Patel <roshanpatel12309@gmail.com>
2025-10-12 20:51:20 +0530

    Modified image uploads

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.get('/',(req,res) => {
     res.send('API Working');
 })
 
-app.use('/api/upload',express.static(path.join(__dirname, "uploads")));
+// app.use('/api/upload',express.static(path.join(__dirname, "uploads")));
 
 app.use('/api/auth',authRouter)
 app.use('/api/roles',roleRouter)
